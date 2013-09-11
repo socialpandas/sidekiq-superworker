9a599dd0c087dcb54d4d66d8fcb86eff969dfc3d
Tom Benner <tombenner@gmail.com>
2013-09-11 16:10:44 -0700

    Fix DSLParser's parsing of nested parallel superworkers

--- lib/sidekiq/superworker/dsl_parser.rb
+++ lib/sidekiq/superworker/dsl_parser.rb
@@ -5,7 +5,8 @@ module Sidekiq
         @dsl_evaluator = DSLEvaluator.new
         @dsl_hash = DSLHash.new
         @record_id = 0
-        block_to_nested_hash(block)
+        nested_hash = block_to_nested_hash(block)
+        @dsl_hash.rewrite_ids_of_nested_hash(nested_hash, 0)
       end
 
       def self.block_to_nested_hash(block)
@@ -17,6 +18,7 @@ module Sidekiq
         while (method_result = fiber.resume)
           method, args, block = method_result
           @record_id += 1
+          method_record_id = @record_id
           if block
             if method == :batch
               nested_hash[@record_id] = { subworker_class: method, arg_keys: args, children: block_to_nested_hash(block) }
@@ -32,13 +34,28 @@ module Sidekiq
           unless [:parallel, :batch].include?(method)
             subworker_class = method.to_s.constantize
             if subworker_class.respond_to?(:is_a_superworker?) && subworker_class.is_a_superworker?
-              parent_record_id = @record_id
-              nested_hash[parent_record_id][:children] = @dsl_hash.rewrite_ids_of_nested_hash(subworker_class.nested_hash, @record_id)
+              children = @dsl_hash.rewrite_ids_of_nested_hash(subworker_class.nested_hash, @record_id)
+              set_children_for_record_id(nested_hash, method_record_id, children)
               @record_id = @dsl_hash.record_id
             end
           end
         end
         nested_hash
+      end
+
+      def self.set_children_for_record_id(nested_hash, parent_record_id, children)
+        nested_hash.each do |record_id, value|
+          if record_id == parent_record_id
+            if nested_hash[record_id][:children].present?
+              nested_hash[record_id][:children] = nested_hash[record_id][:children].reverse_merge(children)
+            else
+              nested_hash[record_id][:children] = children
+            end
+          end
+          if value[:children].present?
+            set_children_for_record_id(value[:children], parent_record_id, children)
+          end
+        end
       end
     end
   end
--- spec/dsl_parser_spec.rb
+++ spec/dsl_parser_spec.rb
@@ -109,5 +109,52 @@ describe Sidekiq::Superworker::DSLParser do
                10=>{:subworker_class=>:Worker9, :arg_keys=>[:first_argument]}}}}
       end
     end
+
+    context 'nested parallel superworker' do
+      it 'returns the correct records' do
+        Sidekiq::Superworker::Worker.create(:Superworker1, :first_argument) do
+          Worker2 :first_argument do
+            Worker3 :first_argument
+          end
+        end
+
+        block = proc do
+          parallel do
+            Worker1 :first_argument
+            Superworker1 :first_argument do
+              parallel do
+                Worker4 :first_argument
+                Worker5 :first_argument
+              end
+            end
+          end
+          Worker1 :first_argument
+        end
+        nested_hash = Sidekiq::Superworker::DSLParser.parse(block)
+        nested_hash.should ==
+          {1=>
+            {:subworker_class=>:parallel,
+             :arg_keys=>[],
+             :children=>
+              {2=>{:subworker_class=>:Worker1, :arg_keys=>[:first_argument]},
+               3=>
+                {:subworker_class=>:Superworker1,
+                 :arg_keys=>[:first_argument],
+                 :children=>
+                  {4=>
+                    {:subworker_class=>:Worker2,
+                     :arg_keys=>[:first_argument],
+                     :children=>
+                      {5=>{:subworker_class=>:Worker3, :arg_keys=>[:first_argument]}}},
+                   6=>
+                    {:subworker_class=>:parallel,
+                     :arg_keys=>[],
+                     :children=>
+                      {7=>{:subworker_class=>:Worker4, :arg_keys=>[:first_argument]},
+                       8=>
+                        {:subworker_class=>:Worker5, :arg_keys=>[:first_argument]}}}}}}},
+           9=>{:subworker_class=>:Worker1, :arg_keys=>[:first_argument]}}
+      end
+    end
   end
 end
