561b16e12dbb2a929ffce6434f7b4f827cdbcb2e
Tom Benner <tombenner@gmail.com>
2014-01-30 22:13:38 -0800

    Only patch in support for jid injection in versions of Sidekiq where it's not already supported

--- lib/sidekiq/client_ext.rb
+++ lib/sidekiq/client_ext.rb
@@ -1,13 +1,17 @@
-# Allow a job's JID to be specified in Sidekiq::Client#push
-module Sidekiq
-  class Client
-    class << self
-      alias_method :original_normalize_item, :normalize_item
+# We need to be able to inject the jid for a job when we call Sidekiq::Client#push.
+# Sidekiq didn't support this prior to 2.12.2, so we need to add support for it to
+# those versions.
+if Gem::Version.new(Sidekiq::VERSION) < Gem::Version.new('2.12.2')
+  module Sidekiq
+    class Client
+      class << self
+        alias_method :original_normalize_item, :normalize_item
 
-      def normalize_item(item)
-        normalized_item = original_normalize_item(item)
-        normalized_item['jid'] = item['jid'] if item['jid'].present?
-        normalized_item
+        def normalize_item(item)
+          normalized_item = original_normalize_item(item)
+          normalized_item['jid'] = item['jid'] if item['jid'].present?
+          normalized_item
+        end
       end
     end
   end
