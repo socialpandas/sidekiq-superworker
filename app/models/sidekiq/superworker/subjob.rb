d92a14dc334985400bfaec2368568742d817539a
Tom Benner <tombenner@gmail.com>
2014-03-25 20:39:06 -0700

    Check for MassAssignmentSecurity when determining whether to call attr_accessible
    AR 4 may require attr_accessible if the protected_attributes gem is installed

--- app/models/sidekiq/superworker/subjob.rb
+++ app/models/sidekiq/superworker/subjob.rb
@@ -3,7 +3,7 @@ module Sidekiq
     class Subjob < ActiveRecord::Base
       attr_accessible :jid, :subjob_id, :superjob_id, :parent_id, :children_ids, :next_id,
         :subworker_class, :superworker_class, :arg_keys, :arg_values, :status, :descendants_are_complete,
-        :meta if ActiveRecord::VERSION::MAJOR < 4
+        :meta if ActiveRecord::VERSION::MAJOR < 4 || ActiveRecord.constants.include?(:MassAssignmentSecurity)
 
       serialize :arg_keys
       serialize :arg_values
